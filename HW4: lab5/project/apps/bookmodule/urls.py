470ab73868a10fc708f068a4ba23ad6bfe29bb12
Jory Aljmal <joryaljmal@gmail.com>
2026-03-03 06:11:15 +0300

    Add files via upload

---
+++ HW4: lab5/project/apps/bookmodule/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+""" urlpatterns=[
+    path('', views.index),
+    path('index2/<int:val1>/', views.index2),
+    path('<int:bookId>', views.viewbook),
+] """
+
+urlpatterns = [
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('html5/links', views.links_view, name="books.html5_links"),
+    path('html5/text/formatting', views.formatting_view, name="books.html5_formatting"),
+    path('html5/listing', views.listing_view, name="books.html5_listing"),
+    path('html5/tables', views.tables_view, name="books.html5_tables"),
+]
